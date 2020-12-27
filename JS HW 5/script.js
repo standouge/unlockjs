952d6e6465bb86466855a2d52a11b3a5fd4ee33b
standouge <75907614+standouge@users.noreply.github.com>
2020-12-26 20:26:34 -0500

    122620

--- JS HW 5/script.js
+++ JS HW 5/script.js
@@ -32,24 +32,41 @@ function showDay() {
       document.getElementById('currentMonth').innerHTML = monthName;
   }
   
-  // function showSeason() {
-  //     var currentSeason = new Date();
-  //     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  //     var monthName = months[currentSeason.getMonth()];
-  //     currentSeason = getSeason(monthName);
-  //     document.getElementById('currentSeason').innerHTML = currentSeason;
-  // }
-  // function getSeason(monthName){
-  //     if(months >= 0 || <= 3){ //asking the question
-  //         return "Winter";
-  //     }else if(months >= 4 || <= 6){
-  //          return "Spring";
-  //     }else if(months >= 7 || <= 9){
-  //         return "Summer";
-  //     }else if(months >= 10 || <= 12){
-  //         return "Fall";
-  //     }else{
-  //         return "Invalid Season"
-  //     }
-  // }      
+  function showSeason() {
+    var month = new Date;
+    var currentMonth = month.getMonth();
+    var seasonMonth = getMonthAsText(currentMonth);
+    document.getElementById('currentSeason').innerHTML = seasonMonth;
+  }
+  function getMonthAsText(seasonMonth){
+     if(currentMonth =>0 || <=3){
+         return 'Winter';
+     }else if(currentMonth =>4 || <=6){
+        return 'Spring';
+     }else if(currentMonth =>7 || <=9){
+        return 'Summer';
+     }else if(currentMonth =>10 || <=12){
+        return 'Fall';
+     }
+  }      
   
+
+
+  //   var currentSeason = new Date();
+    //   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    //   var monthName = months[currentSeason.getMonth()];
+    //   var season = getSeason(monthName);
+    //   document.getElementById('currentSeason').innerHTML = season;
+
+
+    // if(monthName == 'December' || 'January' || 'February'){ 
+    //     return "Winter";
+    // }else if(monthName == 'March' || 'April' || 'May'){
+    //      return "Spring";
+    // }else if(monthName == 'June' || 'July' || 'August'){
+    //     return "Summer";
+    // }else if(monthName == 'September' || 'October' || 'February'){
+    //     return "Fall";
+    // }else{
+    //     return "Invalid Season"
+    // }
